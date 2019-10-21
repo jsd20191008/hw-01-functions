289232f0b39ee007fdd23b0c81c8f79c9b203822
Sissy <cicelybrooks@gmail.com>
2019-10-20 23:20:19 -0400

    first commit

--- pset-functions.js
+++ pset-functions.js
@@ -178,7 +178,19 @@ Use the following test cases to confirm your program meets the success criteria
 console.log('Problem 3:')
 
 // Add your code below this line
+function scrabbleScore(word){
+scrabbleLegend = {a:1, e:1, i:1, o:1, u:1, l:1, n:1, r:1, s:1, t:1, d:2, g:2, b:3, c:3, m:3, p:3, f:4, h:4, v:4, w:4, y:4, k:5, j:8, x:6, q:10, z:10}
+let scrabbleSum = 0;
+for (let i = 0; i < word.length; i++) {
+  scrabbleSum += scrabbleLegend[word.charAt(i)] || 0;
+}
+return scrabbleSum;
+console.log(`Expected Result: ${scrabbleSum}`)
+}
 
+console.log(scrabbleScore("cabbage"));
+console.log(scrabbleScore("javascript"));
+console.log(scrabbleScore("function"));
 // Add your code above this line
 
 /** added for formatting purposes **/
